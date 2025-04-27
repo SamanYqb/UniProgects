387a77faa836de69ad7ec78e9a15293e1a6097f4
SamanYqb <samanyaqubi6155@gmail.com>
2025-04-27 10:12:11 -0700

    Add files via upload

---
+++ index2.java
@@ -0,0 +1,8 @@
+public class index2 {
+    public static void main(String[] args) {
+        System.out.println("Saman Yaqubi");
+        System.out.println("02211142302029");
+        System.out.println("برنامه نویسی موبایل 1");
+
+    }
+}
